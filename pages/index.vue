640e71b4582eeaeee75a3afadb49f06703555506
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-15 20:37:25 -0300

    Feature: Criado a Splashscreen

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div
     class="w-full h-screen bg-orange-500 flex flex-col items-center justify-center px-10 gap-4"
   >
-    <img src="/logo-white.png" />
+    <img src="/logo-white.png" class="max-w-[300px] w-full" />
     <Icon name="line-md:loading-loop" color="#FFF" size="30" />
   </div>
 </template>
