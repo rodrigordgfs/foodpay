c403a1e4fa517766b710337990f748c97c16262f
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-15 20:16:56 -0300

    Feature: Projeto iniciado

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  alias: {
+    "@": path.resolve(__dirname, "./"),
+    "~": path.resolve(__dirname, "./"),
+  },
+  modules: [
+    "nuxt-icon",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
