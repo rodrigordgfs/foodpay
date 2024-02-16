d86d2a72462154723d19a8560676d63e3a9bcdec
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-16 13:08:40 -0300

    Feature: Criado o filtro de procura na Home

--- pages/home.vue
+++ pages/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col w-full h-screen pb-[65px] px-2">
-    <div class="flex flex-row w-full items-center justify-between py-4">
+    <div class="flex flex-row w-full items-center justify-between py-2">
       <LocationSearch />
       <div
         class="group w-10 h-10 flex items-center justify-center rounded-full hover:bg-orange-500 transition-all"
@@ -12,6 +12,37 @@
         />
       </div>
     </div>
+
+    <div class="flex flex-row w-full items-center gap-2">
+      <form @submit.prevent="" class="flex-1">
+        <div class="flex flex-col gap-1">
+          <div
+            class="flex flex-row items-center gap-2 rounded-md px-2 ring-1 ring-orange-500"
+          >
+            <Icon
+              name="material-symbols:search-rounded"
+              size="22"
+              class="text-zinc-400"
+            />
+            <input
+              type="text"
+              class="flex-1 bg-transparent outline-none h-10 placeholder:text-zinc-500 text-sm"
+              placeholder="Procure por comida ou restaurante"
+              v-model="search"
+            />
+          </div>
+        </div>
+      </form>
+      <div
+        class="w-10 h-10 rounded-lg bg-orange-500 flex items-center justify-center hover:bg-orange-600 transition-all cursor-pointer"
+      >
+        <Icon
+          name="material-symbols:filter-alt-outline-sharp"
+          class="text-white"
+          size="24"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +51,8 @@ definePageMeta({
   name: "Home",
   layout: "authenticated",
 });
+
+const search = ref("");
 </script>
 
 <style></style>
