28d16bb8fa1c44252cfeb715438de4f1ff7940c1
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-17 17:22:11 -0300

    Feature: Inicio da criação da tela do restaurante

--- components/Home/Restaurants.vue
+++ components/Home/Restaurants.vue
@@ -13,6 +13,7 @@
         v-for="restaurant in rastaurants"
         :key="restaurant"
         class="shadow flex flex-col rounded-lg cursor-pointer"
+        @click="router.push(`/restaurant/${restaurant.id}`)"
       >
         <img
           :src="restaurant.image"
@@ -69,8 +70,11 @@
 </template>
 
 <script setup>
+const router = useRouter();
+
 const rastaurants = ref([
   {
+    id: "1286ef5d-015f-4f24-9cd1-fa23009b7dfd",
     title: "Burger King",
     image:
       "https://logosmarcas.net/wp-content/uploads/2020/10/Burger-King-Logo-1999-2021.jpg",
@@ -79,6 +83,7 @@ const rastaurants = ref([
     longitude: -51.8303005,
   },
   {
+    id: "7a016638-4d00-44d3-bd68-ffc81128f2ac",
     title: "McDonald's",
     image:
       "https://designportugal.net/wp-content/uploads/2016/04/m-mcdonalds.jpg",
@@ -87,6 +92,7 @@ const rastaurants = ref([
     longitude: -51.8205924,
   },
   {
+    id: "6aa3aee4-8771-4ae5-939f-18cfecd8d78a",
     title: "Burger King",
     image:
       "https://logosmarcas.net/wp-content/uploads/2020/10/Burger-King-Logo-1999-2021.jpg",
@@ -95,6 +101,7 @@ const rastaurants = ref([
     longitude: -51.8166442,
   },
   {
+    id: "dedb46fb-e09f-4198-acd5-04f1df10a7cd",
     title: "McDonald's",
     image:
       "https://designportugal.net/wp-content/uploads/2016/04/m-mcdonalds.jpg",
@@ -103,6 +110,7 @@ const rastaurants = ref([
     longitude: -51.8166442,
   },
   {
+    id: "215de80d-a71b-4fd0-8484-6ba84b0cce73",
     title: "Burger King",
     image:
       "https://logosmarcas.net/wp-content/uploads/2020/10/Burger-King-Logo-1999-2021.jpg",
@@ -111,6 +119,7 @@ const rastaurants = ref([
     longitude: -51.8065591,
   },
   {
+    id: "72810c49-3b7d-43b9-b67d-f33c6d04b1ef",
     title: "McDonald's",
     image:
       "https://designportugal.net/wp-content/uploads/2016/04/m-mcdonalds.jpg",
@@ -119,6 +128,7 @@ const rastaurants = ref([
     longitude: -51.8141122,
   },
   {
+    id: "32b5cc71-22b0-4b0e-9f17-1049c5bf8460",
     title: "Burger King",
     image:
       "https://logosmarcas.net/wp-content/uploads/2020/10/Burger-King-Logo-1999-2021.jpg",
@@ -127,6 +137,7 @@ const rastaurants = ref([
     longitude: -51.8115373,
   },
   {
+    id: "54a2c5c2-0ee9-4685-9854-47db5ccd1d39",
     title: "McDonald's",
     image:
       "https://designportugal.net/wp-content/uploads/2016/04/m-mcdonalds.jpg",
@@ -135,6 +146,7 @@ const rastaurants = ref([
     longitude: -51.8205924,
   },
   {
+    id: "a7fad0ee-7c33-4229-b4df-be4fe77692f1",
     title: "Burger King",
     image:
       "https://logosmarcas.net/wp-content/uploads/2020/10/Burger-King-Logo-1999-2021.jpg",
