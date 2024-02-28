fc1ec5517682b59707a08efa3d9dc33e0876981b
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-28 16:04:42 -0300

    Feature: Ajustado modal de produtos

--- components/ModalFood.vue
+++ components/ModalFood.vue
@@ -4,7 +4,9 @@
     class="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center"
   >
     <OnClickOutside @trigger="modalFood.toggleModal">
-      <div class="bg-white max-w-xl shadow-md p-2 m-5 rounded-2xl">
+      <div
+        class="bg-white dark:bg-zinc-800 max-w-xl shadow-md p-2 m-5 rounded-2xl"
+      >
         <div class="flex flex-col">
           <div class="flex flex-row relative rounded-lg">
             <img
@@ -12,7 +14,7 @@
               class="rounded-tl-lg rounded-tr-lg w-full h-40 shadow-md object-cover"
             />
             <div
-              class="group absolute top-2 left-2 bg-white rounded-lg p-2 h-10 2-10 flex items-center justify-center shadow-md hover:bg-orange-500 transition-all cursor-pointer"
+              class="group absolute top-2 left-2 bg-white dark:bg-zinc-800 rounded-lg p-2 h-10 2-10 flex items-center justify-center shadow-md hover:bg-orange-500 transition-all cursor-pointer"
               @click="toggleFavorite"
             >
               <Icon
@@ -35,7 +37,7 @@
               />
             </div>
             <div
-              class="group absolute bg-white top-2 right-2 rounded-lg p-2 h-10 2-10 flex items-center justify-center shadow-md transition-all cursor-pointer hover:bg-orange-500"
+              class="group absolute bg-white dark:bg-zinc-800 top-2 right-2 rounded-lg p-2 h-10 2-10 flex items-center justify-center shadow-md transition-all cursor-pointer hover:bg-orange-500"
               @click="modalFood.toggleModal"
             >
               <Icon
@@ -45,7 +47,7 @@
               />
             </div>
           </div>
-          <h2 class="font-bold text-2xl text-center my-2">
+          <h2 class="font-bold text-2xl text-center my-2 dark:text-zinc-200">
             {{ modalFood.food.name }}
           </h2>
           <div class="flex flex-row items-center justify-center gap-2">
@@ -55,19 +57,21 @@
                 size="16"
                 class="text-orange-500"
               />
-              <p class="text-sm font-medium">4.5</p>
+              <p class="text-sm font-medium dark:text-zinc-200">4.5</p>
             </div>
             <div class="flex flex-row items-center gap-1">
               <Icon
                 name="material-symbols:alarm"
-                class="text-zinc-600"
+                class="text-zinc-600 dark:text-zinc-400"
                 size="16"
               />
-              <p class="text-sm font-medium">{{ modalFood.food.time }} min</p>
+              <p class="text-sm font-medium dark:text-zinc-200">
+                {{ modalFood.food.time }} min
+              </p>
             </div>
           </div>
           <div class="flex flex-row items-center justify-between my-2">
-            <p class="text-lg font-semibold">
+            <p class="text-lg font-semibold dark:text-zinc-200">
               {{ useFormatMoney(modalFood.food.price) }}
             </p>
             <div
@@ -80,10 +84,10 @@
                 <Icon
                   name="material-symbols:check-indeterminate-small-rounded"
                   size="20"
-                  class="group-hover:text-white transition-all"
+                  class="group-hover:text-white dark:text-zinc-200 transition-all"
                 />
               </div>
-              <p class="font-semibold">{{ quantity }}</p>
+              <p class="font-semibold dark:text-zinc-200">{{ quantity }}</p>
               <div
                 class="group border border-orange-500 w-7 h-7 rounded-full flex items-center justify-center hover:bg-orange-500 transition-all"
                 @click="increaseQuantity"
@@ -91,14 +95,16 @@
                 <Icon
                   name="material-symbols:add-rounded"
                   size="20"
-                  class="group-hover:text-white transition-all"
+                  class="group-hover:text-white dark:text-zinc-200 transition-all"
                 />
               </div>
             </div>
           </div>
           <div class="border-b border-zinc-200 mb-2" />
-          <p class="text-lg font-semibold">Descrição</p>
-          <p class="text-sm">{{ modalFood.food.description }}</p>
+          <p class="text-lg font-semibold dark:text-zinc-200">Descrição</p>
+          <p class="text-sm dark:text-zinc-200">
+            {{ modalFood.food.description }}
+          </p>
           <button
             class="w-full text-white bg-orange-500 py-3 rounded-lg hover:bg-orange-600 transition-all font-semibold disabled:opacity-50 disabled:cursor-not-allowed mt-5"
             @click="handleAddToCart"
